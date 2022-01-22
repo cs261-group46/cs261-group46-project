9827d6efe49074f25bbb843b3dfb87d64970b05b
Jakub Ucinski <kuba200104@gmail.com>
2022-01-22 19:20:24 +0000

    Cookies with flask and react

--- api/api.py
+++ api/api.py
@@ -1,11 +1,55 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+from datetime import datetime, timedelta, timezone
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager
 
-app = Flask(__name__, static_folder="../build", static_url_path="/")
 
-@app.route('/')
-def index():
-    return app.send_static_file("index.html")
+api = Flask(__name__)
 
-@app.route("/api/helloworld")
-def get_current_time():
-    return {"helloworld" : "This is working"}
+api.config["JWT_SECRET_KEY"] = "please-remember-to-change-me"
+api.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+
+jwt = JWTManager(api)
+
+@api.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+
+@api.route('/token', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return {"msg": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+    # response = {"access_token":access_token}
+    resp = make_response({"msg": "cookieSet"})
+    resp.set_cookie('token', access_token)
+    return resp
+
+
+@api.route('/profile')
+@jwt_required()
+def my_profile():
+    response_body = {
+        "name": "Nagato",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    }
+
+    return response_body
+
+@api.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { useEffect, useState } from "react";
+import styles from "./App.css";
+import React, {useState, useEffect} from "react";
 
 const App = () => {
-  const [helloWorldStr, setHelloWorldStr] = useState("");
+  const getCookie = async () => {
+    const response = await fetch("/token", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({
+        "email": "test",
+        "password": "test"
+      })
+    })
 
-  const fetchHelloWorld = async () => {
-    const response = await fetch("/api/helloworld");
-    const data = await response.json();
-    setHelloWorldStr(data.helloworld);
-  };
-
-  useEffect(() => {
-    fetchHelloWorld();
-  }, []);
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+    return data
+  }
+  useEffect(()=> {
+    getCookie()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React : {helloWorldStr}
-        </a>
-      </header>
+    <div className={styles.App}>
+      This works
     </div>
   );
 };
