f0e9602535b36d5d84027f4ac01879fc9c584a60
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-02-09 20:27:22 +0000

    added react-router integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css';
-import Homepage from "./components/Homepage/Homepage.lazy";
+import Homepage from "./routes/Homepage/Homepage.lazy";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-      <Homepage/>
+    <BrowserRouter>
+      <Routes>
+        {/* webpage index */}
+        <Route path="/" element={<Homepage/>}/>
+
+        {/* catch all 404 page if nothing matches */}
+        <Route path="*" element={<p>404!</p>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
