87ff081e8661887726781dc916e70742915448fc
Jakub Ucinski <kuba200104@gmail.com>
2022-02-11 15:41:04 +0000

    useInput hook added

---
+++ src/hooks/UseInput/UseInput.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import UseInput from './UseInput';
+
+describe('<UseInput />', () => {
+  test('it should mount', () => {
+    render(<UseInput />);
+    
+    const useInput = screen.getByTestId('UseInput');
+
+    expect(useInput).toBeInTheDocument();
+  });
+});
