c35d3ae02bdd6690a37792341c18b5d96a8b1136
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-02-16 16:34:51 +0000

    added icons to buttons

--- src/routes/Dashboard/Dashboard.tsx
+++ src/routes/Dashboard/Dashboard.tsx
@@ -9,11 +9,47 @@ const Dashboard: FC<DashboardProps> = () => (
     <MainLayout title={"Dashboard"}>
         <h1>Welcome back!</h1>
 
-        <Button icon={"🧍"} onClick={() => {}}>
-            <p>Your Profile</p>
-        </Button>
+        <Button icon={"👤"} onClick={() => {}}>Your Profile</Button>
 
+        <Button icon={"🔔"} onClick={() => {}}>All Notifications</Button>
 
+        <Button icon={"📅"} onClick={() => {}}>Upcoming Events</Button>
+
+        <Button icon={"💬"} onClick={() => {}}>Messages</Button>
+
+        <Button icon={"⚙️"} onClick={() => {}}>Settings</Button>
+
+        <Button icon={"👋"} onClick={() => {}}>Logout</Button>
+
+        <h1>Your Learning</h1>
+
+        <Button icon={"🔔"} onClick={() => {}}>Recent Notifications</Button>
+
+        <Button icon={"👨‍🏫"} onClick={() => {}}>Your Mentor</Button>
+
+        <Button icon={"📈"} onClick={() => {}}>Plans of Action</Button>
+
+        <Button icon={"✏️"} onClick={() => {}}>Workshops</Button>
+
+        <Button icon={"👥"} onClick={() => {}}>Group Sessions</Button>
+
+        <Button icon={"💡"} onClick={() => {}}>Your Interests</Button>
+
+        <h1>Your Mentoring</h1>
+
+        <Button icon={"🔔"} onClick={() => {}}>Recent Notifications</Button>
+
+        <Button icon={"🧑‍🎓"} onClick={() => {}}>Your Mentees</Button>
+
+        <Button icon={"💪"} onClick={() => {}}>Your Skills</Button>
+
+        <h1>Your Expertise</h1>
+
+        <Button icon={"🔔"} onClick={() => {}}>Recent Notifications</Button>
+
+        <Button icon={"✏"} onClick={() => {}}>Your Workshops</Button>
+
+        <Button icon={"💪"} onClick={() => {}}>Your Fields of Expertise</Button>
 
         <div data-testid="Dashboard"/>
     </MainLayout>
