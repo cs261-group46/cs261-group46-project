be2e9da94a6eb9c48053b5d07502b4fd282fe701
Mohammed Kimti <mohammedkimti@Mohammeds-MacBook-Pro.local>
2022-02-18 15:24:28 +0000

    verify register and expert pages added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {Routes, BrowserRouter, Route} from "react-router-dom";
 import './App.scss';
 import Register from "./routes/Register/Register.lazy";
 import Dashboard from "./routes/Dashboard/Dashboard.lazy";
+import VerifyRegister from "./routes/VerifyRegister/VerifyRegister.lazy"
+import Expert from "./routes/Expert/Expert.lazy"
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
             <Routes>
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/verifyregister" element={<VerifyRegister/>}/>
+                <Route path="/expert" element={<Expert/>}/>
             </Routes>
         </div>
       </BrowserRouter>
---
+++ src/routes/VerifyRegister/VerifyRegister.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import MainLayout from "../../layouts/MainLayout/MainLayout";
+import Button from "../../components/UI/Button/Button";
+import styles from './VerifyRegister.module.scss';
+
+function VerifyRegister() {
+    return (
+        <div>
+            <MainLayout title="Register">
+        </MainLayout>
+        <h1 className={styles.h1}>We've sent you an email.</h1>
+        <h2 className={styles.h2}>Please confirm your email by clicking the link provided.</h2>
+        <h3 className={styles.h3}>Didn't recieve an email?</h3>
+        <a className={styles.a} href="/dashboard">Resend</a>
+        </div>
+    );
+}
+
+export default VerifyRegister;
