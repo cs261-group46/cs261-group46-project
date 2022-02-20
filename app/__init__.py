be93b9fa8e372edb41300eff0826e4ec76c31d53
Jakub Ucinski <kuba200104@gmail.com>
2022-02-20 12:07:23 +0000

    fixed dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -20,49 +20,25 @@
 from flask import Flask, request, session, redirect, url_for, send_file, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
-from app.environ import get
-
-# import app.FileManager as FileManager
-# import app.config as Config
-# import app.SQL as SQL
-# import app.user as Users
-# from app.ouremail import EMail as Mail
-
-# peper = environ.get('PEPER')
-# name = environ.get('NAME')
+from dotenv import load_dotenv
 
 ##########################
 # Config Options/Loading #
 ##########################
 
-# login_token_key_str = "login_token"
-
-
-# sql_config   = Config.load_config("sql")
-# email_config = Config.load_config("ouremail")
-# db = SQL.open_connection(sql_config["connection"])
-
-# for sql_file in sql_config["sql_files"]:
-#     print(f"Loading SQL file {sql_file}")
-#     SQL.load_defaults_2(db, FileManager.read_file("sql", sql_file))
-
 #############
 # App setup #
 #############
-
+load_dotenv()
 
 app = Flask(__name__, static_folder="../build/static/", static_url_path="/static")
-app.config["SECRET_KEY"] = get('SECRET_KEY')
-db_user = get('DATABASE_USER')
-db_password = get('DATABASE_PASSWORD')
-db_database = get('DATABASE_NAME')
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+db_user = os.getenv('DATABASE_USER')
+db_password = os.getenv('DATABASE_PASSWORD')
+db_database = os.getenv('DATABASE_NAME')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@localhost:5432/{db_database}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-
-
-
 
 # if email_config["enabled"]:
 #     for key, value in email_config["email_server"].items():
--- app/models.old/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models import topic as Topics
-from app.models import departments as Departments
-from app.models.topic import Topic
-from app.models.departments import Department
-from app.models.ConcurrentContext import ConcurrentContext
