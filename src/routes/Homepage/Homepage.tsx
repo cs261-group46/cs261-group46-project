3eb7f5bb0a6e91a4cd3bc7b6b1a91159394ee06f
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-02-22 15:06:55 +0000

    updated homepage to redirect to dashboard on being logged in

--- src/routes/Homepage/Homepage.tsx
+++ src/routes/Homepage/Homepage.tsx
@@ -1,11 +1,22 @@
-import React, { FC } from 'react';
+import React, {FC, useEffect} from 'react';
 import styles from './Homepage.module.scss';
 import Button from "../../components/UI/Button/Button";
 import Logo from "../../logo";
+import useLogin from "../../hooks/UseLogin/UseLogin";
+import {useNavigate} from "react-router-dom";
 
 interface HomepageProps {}
 
 const Homepage: FC<HomepageProps> = () => {
+  const navigate = useNavigate();
+  const loginState = useLogin();
+
+  useEffect(() => {
+    if (loginState.state === "logged_in") {
+      navigate("/dashboard");
+    }
+  }, [loginState, navigate]);
+
   return <div className={styles.Homepage} data-testid="Homepage">
     <Logo scale={0.5}/>
     <div className={styles.buttons}>
