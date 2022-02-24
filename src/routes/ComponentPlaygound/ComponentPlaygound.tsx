9af475142c61a5b76d4b0bf4eebbd0effcbf6e25
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-02-24 23:46:01 +0000

    added hour input to playground

--- src/routes/ComponentPlaygound/ComponentPlaygound.tsx
+++ src/routes/ComponentPlaygound/ComponentPlaygound.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import styles from './ComponentPlaygound.module.scss';
+import HoursInput from "../../components/UI/FormInput/HoursInput/HoursInput";
 
 interface ComponentPlaygoundProps {}
 
 const ComponentPlaygound: FC<ComponentPlaygoundProps> = () => (
     <div className={styles.ComponentPlaygound} data-testid="ComponentPlaygound">
-
+        <HoursInput label={"hour"} isValid={true} value={[]} onChange={() => {}} onBlur={() => {}}/>
     </div>
 );
 
