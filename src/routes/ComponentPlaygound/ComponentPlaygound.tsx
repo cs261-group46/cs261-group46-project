24be92ce314cc021e5705d53014a7a1314572ccd
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-02-24 23:31:33 +0000

    made page stepper function

--- src/routes/ComponentPlaygound/ComponentPlaygound.tsx
+++ src/routes/ComponentPlaygound/ComponentPlaygound.tsx
@@ -1,12 +1,15 @@
-import React, { FC } from 'react';
+import React, {FC, useState} from 'react';
 import styles from './ComponentPlaygound.module.scss';
+import PageStepper from "../../components/UI/PageStepper/PageStepper";
 
 interface ComponentPlaygoundProps {}
 
-const ComponentPlaygound: FC<ComponentPlaygoundProps> = () => (
-    <div className={styles.ComponentPlaygound} data-testid="ComponentPlaygound">
+const ComponentPlaygound: FC<ComponentPlaygoundProps> = () => {
+    const [page, setPage] = useState(0);
 
+    return <div className={styles.ComponentPlaygound} data-testid="ComponentPlaygound">
+        <PageStepper page={page} setPage={setPage} maxPages={5}/>
     </div>
-);
+}
 
 export default ComponentPlaygound;
