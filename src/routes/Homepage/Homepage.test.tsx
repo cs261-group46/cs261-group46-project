347a62819d11bf8a0988d47bbf1f41759c087d89
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-02-28 11:30:56 +0000

    fix tests

--- src/routes/Homepage/Homepage.test.tsx
+++ src/routes/Homepage/Homepage.test.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Homepage from './Homepage';
+import {BrowserRouter} from "react-router-dom";
 
 describe('<Homepage />', () => {
   test('it should mount', () => {
-    render(<Homepage />);
+    render(<BrowserRouter><Homepage /></BrowserRouter>);
     
     const homepage = screen.getByTestId('Homepage');
 
