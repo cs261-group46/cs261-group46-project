86711491d28276c81c30659706d241906fdc986c
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-03-03 22:24:36 +0000

    brought CreateWorkshop up to scratch witb other forms' standards

--- src/routes/CreateWorkshop/CreateWorkshop.tsx
+++ src/routes/CreateWorkshop/CreateWorkshop.tsx
@@ -1,183 +1,200 @@
-import React, { FC } from 'react';
-import styles from './CreateWorkshop.module.scss';
+import React, { FC, FormEventHandler } from "react";
+import styles from "./CreateWorkshop.module.scss";
 import MainLayout from "../../layouts/MainLayout/MainLayout";
 import SearchSelect from "../../components/UI/FormInput/SearchSelect/SearchSelect";
-import {MultiSelectOptions, SearchPromise} from "../../components/UI/FormInput/SearchSelect/SearchSelect.d";
+import {
+  MultiSelectOptions,
+  SearchPromise,
+} from "../../components/UI/FormInput/SearchSelect/SearchSelect.d";
 import useInput from "../../hooks/UseInput/UseInput";
 import DatePicker from "../../components/UI/FormInput/DatePicker/DatePicker";
-import HoursInput, {Range} from "../../components/UI/FormInput/HoursInput/HoursInput";
+import HoursInput, {
+  Range,
+} from "../../components/UI/FormInput/HoursInput/HoursInput";
 import BigTextInput from "../../components/UI/FormInput/BigTextInput/BigTextInput";
 import TextInput from "../../components/UI/FormInput/TextInput/TextInput";
 import Select from "../../components/UI/FormInput/Select/Select";
-import {SelectOption} from "../../components/UI/FormInput/Select/Select.d";
-import SystemMessage from "../../components/UI/SystemMessage/SystemMessage";
+import { SelectOption } from "../../components/UI/FormInput/Select/Select.d";
 import Button from "../../components/UI/Button/Button";
+import UseVerifyAuth from "../../hooks/UseVerifyAuth/UseVerifyAuth";
 
 interface CreateWorkshopProps {}
 
 interface Room {
-    id: number;
-    label: string;
+  id: number;
+  label: string;
 }
 
 const CreateWorkshop: FC<CreateWorkshopProps> = () => {
-    const {
-        enteredValue: room,
-        changeHandler: roomChangeHandler,
-        blurHandler: roomBlurHandler,
-        isInputValid: roomValid,
-    } = useInput<MultiSelectOptions<Room>>(value => value.length > 0, []);
+  UseVerifyAuth();
 
-    const {
-        enteredValue: invite,
-        changeHandler: inviteChangeHandler,
-        blurHandler: inviteBlurHandler,
-    } = useInput<string>(() => true, "");
+  const {
+    enteredValue: room,
+    changeHandler: roomChangeHandler,
+    blurHandler: roomBlurHandler,
+    isInputValid: roomInputValid,
+    isValueValid: roomValueValid,
+  } = useInput<MultiSelectOptions<Room>>([], (value) => value.length > 0);
 
-    const {
-        enteredValue: date,
-        changeHandler: dateChangeHandler,
-        blurHandler: dateBlurHandler
-    } = useInput<Date>(() => true, new Date());
+  const {
+    enteredValue: invite,
+    changeHandler: inviteChangeHandler,
+    blurHandler: inviteBlurHandler,
+  } = useInput<string>("");
 
-    const {
-        enteredValue: time,
-        changeHandler: timeChangeHandler,
-        blurHandler: timeBlurHandler,
-        isInputValid: timeValid,
-    } = useInput<Range[]>(value => value.length === 1, []);
+  const {
+    enteredValue: date,
+    changeHandler: dateChangeHandler,
+    blurHandler: dateBlurHandler,
+  } = useInput<Date>(new Date());
 
-    const {
-        enteredValue: description,
-        changeHandler: descriptionChangeHandler,
-        blurHandler: descriptionBlurHandler
-    } = useInput<string>(() => true, "");
+  const {
+    enteredValue: time,
+    changeHandler: timeChangeHandler,
+    blurHandler: timeBlurHandler,
+    isInputValid: timeInputValid,
+    isValueValid: timeValueValid,
+  } = useInput<Range[]>([]);
 
-    const {
-        enteredValue: capacity,
-        changeHandler: capacityChangeHandler,
-        isInputValid: capacityValid,
-        blurHandler: capacityBlurHandler
-    } = useInput<number>((value) => value > 0, 0);
+  const {
+    enteredValue: description,
+    changeHandler: descriptionChangeHandler,
+    blurHandler: descriptionBlurHandler,
+  } = useInput<string>("");
 
-    const {
-        enteredValue: type,
-        changeHandler: typeChangeHandler,
-        blurHandler: typeBlurHandler
-    } = useInput<SelectOption<string>>((value) => value.id !== "", {id: ""});
+  const {
+    enteredValue: capacity,
+    changeHandler: capacityChangeHandler,
+    isInputValid: capacityInputValid,
+    isValueValid: capacityValueValid,
+    blurHandler: capacityBlurHandler,
+  } = useInput<number>(0, (value) => value > 0);
 
-    const searchPromise: SearchPromise<Room> = async search => {
-        const result = await fetch(`/api/meetings/rooms?startwith=${search}`);
+  const {
+    enteredValue: type,
+    changeHandler: typeChangeHandler,
+    blurHandler: typeBlurHandler,
+  } = useInput<SelectOption<string>>(
+    { value: "" },
+    (value) => value.value !== ""
+  );
 
-        if (result.ok) {
-            const rooms: {result: Room[]} = await result.json();
+  const searchPromise: SearchPromise<Room> = async (search) => {
+    const result = await fetch(`/api/meetings/rooms?startwith=${search}`);
 
-            return rooms.result.map(room => ({label: room.label, value: room}));
-        }
+    if (result.ok) {
+      const rooms: { result: Room[] } = await result.json();
 
-        // else if bad, do nothing
-        return [];
-    };
-
-    const registrationHandler = () => {
-        // ensure all the values are valid by forcing a blur
-        // this will also make errors show if needed
-        roomBlurHandler();
-        timeBlurHandler();
-        capacityBlurHandler();
-
-        if (
-            roomValid &&
-            timeValid &&
-            capacityValid
-        ) {
-            // send off this event to the backend
-        }
+      return rooms.result.map((room) => ({ label: room.label, value: room }));
     }
 
-    return <MainLayout title={"Create a Workshop"}>
-        <div className={styles.CreateWorkshop} data-testid="CreateWorkshop">
-            <SearchSelect
-                id={"room"}
-                label={"Room"}
-                isValid={true}
-                value={room}
-                onChange={roomChangeHandler}
-                onBlur={roomBlurHandler}
-                searchPromise={searchPromise}
-                limit={1}
-            />
+    // else if bad, do nothing
+    return [];
+  };
 
-            <SystemMessage sort={"inline"} type={"alert"} description={"Please select a room"} visible={!roomValid} noX/>
+  function showErrors() {
+    // this will also make errors show if needed
+    roomBlurHandler();
+    timeBlurHandler();
+    capacityBlurHandler();
+  }
 
-            <TextInput
-                id={"invite"}
-                label={"Alternative Invite URL"}
-                placeholder={"e.g. a Virtual Meeting"}
-                value={invite}
-                isValid={true}
-                onChange={inviteChangeHandler}
-                onBlur={inviteBlurHandler}
-            />
+  const submitHandler: FormEventHandler = (event) => {
+    event.preventDefault();
+    if (roomValueValid && timeValueValid && capacityValueValid) {
+      // send off this event to the backend
+    } else {
+      showErrors();
+    }
+  };
 
-            <DatePicker
-                id={"date"}
-                label={"Date"}
-                value={date}
-                isValid={true}
-                onChange={dateChangeHandler}
-                onBlur={dateBlurHandler}
-            />
+  return (
+    <MainLayout title={"Create a Workshop"}>
+      <form
+        className={styles.CreateWorkshop}
+        data-testid="CreateWorkshop"
+        onSubmit={submitHandler}
+      >
+        <SearchSelect
+          id={"room"}
+          label={"Room"}
+          isValid={roomInputValid}
+          value={room}
+          onChange={roomChangeHandler}
+          onBlur={roomBlurHandler}
+          searchPromise={searchPromise}
+          limit={1}
+        />
 
-            <HoursInput
-                label={"Time"}
-                isValid={true}
-                value={time}
-                onChange={timeChangeHandler}
-                onBlur={timeBlurHandler}
-                width={"150px"}
-            />
+        <TextInput
+          id={"invite"}
+          label={"Alternative Invite URL"}
+          placeholder={"e.g. a Virtual Meeting"}
+          value={invite}
+          isValid={true}
+          onChange={inviteChangeHandler}
+          onBlur={inviteBlurHandler}
+        />
 
-            <SystemMessage sort={"inline"} type={"alert"} description={"Please select 1 range of times"} visible={!timeValid} noX/>
+        <DatePicker
+          id={"date"}
+          label={"Date"}
+          value={date}
+          isValid={true}
+          onChange={dateChangeHandler}
+          onBlur={dateBlurHandler}
+        />
 
-            <BigTextInput
-                id={"description"}
-                label={"Description"}
-                placeholder={"Tell attendees what the event is about"}
-                value={description}
-                isValid={true}
-                onChange={descriptionChangeHandler}
-                onBlur={descriptionBlurHandler}/>
-            
-            <TextInput
-                id={"capacity"}
-                label={"Capacity"}
-                placeholder={""}
-                value={capacity.toString()}
-                isValid={capacityValid}
-                type={"number"}
-                onChange={newValue => capacityChangeHandler(parseInt(newValue))}
-                onBlur={capacityBlurHandler}
-            />
+        <HoursInput
+          label={"Time"}
+          isValid={timeInputValid}
+          value={time}
+          onChange={timeChangeHandler}
+          onBlur={timeBlurHandler}
+          width={"150px"}
+        />
 
-            <SystemMessage sort={"inline"} type={"alert"} description={"Capacity must be greater than 0"} visible={!capacityValid} noX/>
+        <BigTextInput
+          id={"description"}
+          label={"Description"}
+          placeholder={"Tell attendees what the event is about"}
+          value={description}
+          isValid={true}
+          onChange={descriptionChangeHandler}
+          onBlur={descriptionBlurHandler}
+        />
 
-            <Select
-                id={"type"}
-                label={"Meeting Type"}
-                placeholder={"Public or Private?"}
-                options={[{id: "public", label: "Public (visible on Workshops)"}, {id: "private", label: "Private (requires a link)"}]}
-                value={type}
-                isValid={true}
-                onChange={typeChangeHandler}
-                onBlur={typeBlurHandler}
-            />
+        <TextInput
+          id={"capacity"}
+          label={"Capacity"}
+          placeholder={""}
+          value={capacity.toString()}
+          isValid={capacityInputValid}
+          type={"number"}
+          onChange={(newValue) => capacityChangeHandler(parseInt(newValue))}
+          onBlur={capacityBlurHandler}
+        />
 
-            <Button icon="👑" onClick={registrationHandler} buttonStyle={"primary"}>
-                Register
-            </Button>
-        </div>
+        <Select
+          id={"type"}
+          label={"Meeting Type"}
+          placeholder={"Public or Private?"}
+          options={[
+            { value: "public", label: "Public (visible on Workshops)" },
+            { value: "private", label: "Private (requires a link)" },
+          ]}
+          value={type}
+          isValid={true}
+          onChange={typeChangeHandler}
+          onBlur={typeBlurHandler}
+        />
+
+        <Button icon="👑" type={"submit"} buttonStyle={"primary"}>
+          Register
+        </Button>
+      </form>
     </MainLayout>
-}
+  );
+};
 
 export default CreateWorkshop;
