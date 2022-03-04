99bbf7918fc14eb547ff20640fb599b399e2c6eb
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-03-04 09:41:29 +0000

    reformatted file

--- src/components/UI/FormInput/HoursInput/ClockInput.tsx
+++ src/components/UI/FormInput/HoursInput/ClockInput.tsx
@@ -1,4 +1,4 @@
-import React, {FC, MouseEvent, useState} from "react";
+import React, { FC, MouseEvent, useState } from "react";
 import styles from "./ClockInput.module.scss";
 import segmentHighlight from "./segment_highlight.svg";
 import segment from "./segment.svg";
@@ -23,27 +23,34 @@ const ClockInput: FC<ClockInputProps> = (props) => {
   const getClickedIndex = (event: MouseEvent<HTMLImageElement>) => {
     const rect = event.currentTarget.getBoundingClientRect();
     // centre x and y to middle of the element
-    const x = (event.clientX - rect.left) - (rect.width / 2);
-    const y = (event.clientY - rect.top) - (rect.height / 2);
+    const x = event.clientX - rect.left - rect.width / 2;
+    const y = event.clientY - rect.top - rect.height / 2;
     // uses atan2 to convert to an angle and does some processing to allign it to the slot correctly
-    return (12 - Math.ceil((Math.atan2(x, y) / (Math.PI / 6)) + 6));
-  }
+    return 12 - Math.ceil(Math.atan2(x, y) / (Math.PI / 6) + 6);
+  };
 
   const flip = (i: number) => {
-    props.onChange(props.value.map((bool, replaceIndex) => i === replaceIndex ? !bool : bool))
-  }
+    props.onChange(
+      props.value.map((bool, replaceIndex) =>
+        i === replaceIndex ? !bool : bool
+      )
+    );
+  };
 
-  return <div className={styles.ClockInput}>
-    {
-      // reversed so slice 0 is on top - leads to easier math
-      Array.from(Array(12).keys()).reverse().map(i =>
-          <img
+  return (
+    <div className={styles.ClockInput}>
+      {
+        // reversed so slice 0 is on top - leads to easier math
+        Array.from(Array(12).keys())
+          .reverse()
+          .map((i) => (
+            <img
               src={props.value[i] ? segmentHighlight : segment}
               width={props.width}
               key={i}
               className={styles.segment}
               alt="Clock Face"
-              style={{rotate: `${i * 30}deg`}}
+              style={{ rotate: `${i * 30}deg` }}
               onMouseDown={(event) => {
                 setMouseDown(true);
                 // store whether the initial segment was on/off.
@@ -63,16 +70,15 @@ const ClockInput: FC<ClockInputProps> = (props) => {
                     flip(clickedIndex);
                 }
               }}
-
               // need to stop image dragging
               onDragStart={(event) => event.preventDefault()}
-          />
-      )
-    }
-    <p className={styles.label}>{props.label}</p>
-    <img src={clockFace} alt="Clock Face" width={props.width}/>
-  </div>
-}
+            />
+          ))
+      }
+      <p className={styles.label}>{props.label}</p>
+      <img src={clockFace} alt="Clock Face" width={props.width} />
+    </div>
+  );
+};
 
-
-export default ClockInput
+export default ClockInput;
