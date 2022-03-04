fd9bd74a581f8c7535ad2e2cdaa6d7dbf02ac193
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-03-04 09:47:40 +0000

    reformatted file

--- src/routes/ComponentPlaygound/ComponentPlaygound.tsx
+++ src/routes/ComponentPlaygound/ComponentPlaygound.tsx
@@ -1,5 +1,5 @@
-import React, {FC, useState} from 'react';
-import styles from './ComponentPlaygound.module.scss';
+import React, { FC, useState } from "react";
+import styles from "./ComponentPlaygound.module.scss";
 import PageStepper from "../../components/UI/PageStepper/PageStepper";
 import PieView from "../../components/UI/PieView/PieView";
 import HoursInput from "../../components/UI/FormInput/HoursInput/HoursInput";
@@ -10,41 +10,59 @@ import useInput from "../../hooks/UseInput/UseInput";
 interface ComponentPlaygoundProps {}
 
 const ComponentPlaygound: FC<ComponentPlaygoundProps> = () => {
-    const [page, setPage] = useState(0);
-    const {
-        enteredValue: searchValue,
-        changeHandler: searchChange,
-        blurHandler: searchBlur
-    } = useInput<MultiSelectOptions<string>>([], () => true)
+  const [page, setPage] = useState(0);
+  const {
+    enteredValue: searchValue,
+    changeHandler: searchChange,
+    blurHandler: searchBlur,
+  } = useInput<MultiSelectOptions<string>>([], () => true);
 
-    return <div className={styles.ComponentPlaygound} data-testid="ComponentPlaygound">
-        <PageStepper page={page} setPage={setPage} maxPages={5}/>
-        <PieView segments={[
-            {fr: 2, fill: "#07A417", label: "Active"},
-            {fr: 1, fill: "#393838", label: "Completed"}
-        ]}/>
-        <HoursInput label={"hour"} isValid={true} value={[]} onChange={() => {}} onBlur={() => {}}/>
+  return (
+    <div className={styles.ComponentPlaygound} data-testid="ComponentPlaygound">
+      <PageStepper page={page} setPage={setPage} maxPages={5} />
+      <PieView
+        segments={[
+          { fr: 2, fill: "#07A417", label: "Active" },
+          { fr: 1, fill: "#393838", label: "Completed" },
+        ]}
+      />
+      <HoursInput
+        label={"hour"}
+        isValid={true}
+        value={[]}
+        onChange={() => {}}
+        onBlur={() => {}}
+      />
 
-        <SearchSelect
-            id={"search"}
-            label={"Test"}
-            isValid={true}
-            value={searchValue}
-            onChange={searchChange}
-            onBlur={searchBlur}
-            type={"draggable"}
-            searchPromise={_value => new Promise(resolve => setTimeout(() => resolve([
-                {
+      <SearchSelect
+        id={"search"}
+        label={"Test"}
+        isValid={true}
+        value={searchValue}
+        onChange={searchChange}
+        onBlur={searchBlur}
+        type={"draggable"}
+        searchPromise={(_value) =>
+          new Promise((resolve) =>
+            setTimeout(
+              () =>
+                resolve([
+                  {
                     label: "Test 1",
-                    value: "1"
-                },
-                {
+                    value: "1",
+                  },
+                  {
                     label: "Test 2",
-                    value: "2"
-                },
-            ]), 0))}
-        />
+                    value: "2",
+                  },
+                ]),
+              0
+            )
+          )
+        }
+      />
     </div>
-}
+  );
+};
 
 export default ComponentPlaygound;
