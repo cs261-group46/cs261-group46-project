de62678ca6966831656bc9eebc3133d5a83c7df8
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-03-06 11:41:54 +0000

    add tests for notifications

--- src/components/Notifications/Notifications.test.tsx
+++ src/components/Notifications/Notifications.test.tsx
@@ -5,18 +5,47 @@ import Notifications from "./Notifications";
 import { NotificationType } from "../../types/Notification";
 
 describe("<Notifications />", () => {
+  const testNotification: NotificationType<string> = {
+    id: 0,
+    notification_type: "type",
+    notification_level: "warning",
+    description: "Test Notification",
+  };
+
+  var notificationsList: NotificationType<string>[] = [testNotification];
+  const removeNotification = (notification: NotificationType<string>) =>
+    notificationsList.splice(notificationsList.indexOf(notification));
+
+  const notificationsElement = (
+    <Notifications
+      notifications={notificationsList}
+      onRemove={removeNotification}
+    />
+  );
+
   test("it should mount", () => {
-    render(
-      <Notifications
-        notifications={[]}
-        onRemove={function (notification: NotificationType<string>): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
-    );
+    render(notificationsElement);
 
     const notifications = screen.getByTestId("Notifications");
 
     expect(notifications).toBeInTheDocument();
+  });
+
+  it("should display notification test", () => {
+    render(notificationsElement);
+
+    const notifications = screen.getByTestId("Notifications");
+
+    expect(notifications).toHaveTextContent("Test Notification");
+  });
+
+  it("should not display notification when function is called", () => {
+    removeNotification(testNotification);
+
+    render(notificationsElement);
+
+    const notifications = screen.getByTestId("Notifications");
+
+    expect(notifications).not.toHaveTextContent("Test Notification");
   });
 });
