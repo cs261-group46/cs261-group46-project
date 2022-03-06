d1f1019da6e476b84ca1d55881225dd97fc784c9
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-03-06 11:28:23 +0000

    add tests for MentorCard

--- src/components/learn/MentorIndex/MentorCard/MentorCard.test.tsx
+++ src/components/learn/MentorIndex/MentorCard/MentorCard.test.tsx
@@ -3,29 +3,59 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import MentorCard from "./MentorCard";
 
+const mentor = {
+  id: 0,
+  about: "",
+  topics: [],
+  user: {
+    department: {
+      id: 0,
+      name: "test0",
+    },
+    first_name: "test",
+    last_name: "test2",
+    email: "test3@gmail.com",
+  },
+};
+
 describe("<MentorCard />", () => {
   test("it should mount", () => {
-    render(
-      <MentorCard
-        mentor={{
-          id: 0,
-          about: "",
-          topics: [],
-          user: {
-            department: {
-              id: 0,
-              name: "",
-            },
-            first_name: "",
-            last_name: "",
-            email: "",
-          },
-        }}
-      />
-    );
+    render(<MentorCard mentor={mentor} />);
 
     const mentorCard = screen.getByTestId("MentorCard");
 
     expect(mentorCard).toBeInTheDocument();
+  });
+
+  it("contains the mentors first name", () => {
+    render(<MentorCard mentor={mentor} />);
+
+    const mentorCard = screen.getByTestId("MentorCard");
+
+    expect(mentorCard).toHaveTextContent("test1");
+  });
+
+  it("contains the mentors last name", () => {
+    render(<MentorCard mentor={mentor} />);
+
+    const mentorCard = screen.getByTestId("MentorCard");
+
+    expect(mentorCard).toHaveTextContent("test2");
+  });
+
+  it("contains the mentors email", () => {
+    render(<MentorCard mentor={mentor} />);
+
+    const mentorCard = screen.getByTestId("MentorCard");
+
+    expect(mentorCard).toHaveTextContent("test3");
+  });
+
+  it("contains the mentors department", () => {
+    render(<MentorCard mentor={mentor} />);
+
+    const mentorCard = screen.getByTestId("MentorCard");
+
+    expect(mentorCard).toHaveTextContent("test0");
   });
 });
