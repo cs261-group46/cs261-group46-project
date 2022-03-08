d87a6805515d15bb6e4b90c6b1b0e227bffbf39e
samuelyoung-uk <samuel.ld.young@gmail.com>
2022-03-08 20:30:14 +0000

    third pass of changes

--- src/components/UI/FormInput/DatePicker/DatePicker.tsx
+++ src/components/UI/FormInput/DatePicker/DatePicker.tsx
@@ -1,24 +1,45 @@
-import React, { FC } from 'react';
-import styles from './DatePicker.module.scss';
+import React, { FC, useEffect, useState } from "react";
+import styles from "./DatePicker.module.scss";
 import BaseDatePicker from "react-datepicker";
-import './datepicker.scss';
+import "./datepicker.scss";
 import Label from "../Label/Label";
+import SystemMessage from "../../SystemMessage/SystemMessage";
 
 interface DatePickerProps {
-    id: string;
-    label: string;
-    icon?: React.ReactNode;
-    value: Date;
-    isValid: boolean;
-    onChange: (input: Date) => void;
-    onBlur: () => void;
+  id: string;
+  label: string;
+  icon?: React.ReactNode;
+  value: Date;
+  isValid: boolean;
+  onChange: (input: Date) => void;
+  onBlur: () => void;
 }
 
-const DatePicker: FC<DatePickerProps> = (props) => (
+const DatePicker: FC<DatePickerProps> = (props) => {
+  const [isInvalidMessageVisible, setInvalidMessageVisible] = useState(false);
+
+  useEffect(() => {
+    setInvalidMessageVisible(!props.isValid);
+  }, [props.isValid]);
+
+  return (
     <div className={styles.DatePicker} data-testid="DatePicker">
-        <Label htmlFor={props.id} icon={props.icon}>{props.label}</Label>
-        <BaseDatePicker onChange={props.onChange} selected={props.value} onBlur={props.onBlur}/>
+      <Label htmlFor={props.id} icon={props.icon}>
+        {props.label}
+      </Label>
+      <BaseDatePicker
+        onChange={props.onChange}
+        selected={props.value}
+        onBlur={props.onBlur}
+      />
+      <SystemMessage
+        sort="inline"
+        type="alert"
+        description={`The ${props.label} field seems to be incorrect`}
+        visible={isInvalidMessageVisible}
+      />
     </div>
-);
+  );
+};
 
 export default DatePicker;
