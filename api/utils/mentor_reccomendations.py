d4cc1b03290eed5fafb076f6f9b31c6f797da56d
Jakub Ucinski <kuba200104@gmail.com>
2022-03-13 11:41:55 +0000

    found bug in RBO

--- api/utils/mentor_reccomendations.py
+++ api/utils/mentor_reccomendations.py
@@ -5,18 +5,16 @@ def get_mentors(mentors, user):
         if mentor.user.department.id != user.department.id and mentor.user.id != user.id and len(mentor.mentees) < mentor.capacity:
             suitable_mentors.append(mentor)
 
-    user_topics = [topic.topic for topic in sorted(user.mentee.topics, key=lambda topic: topic.priority)]
-
+    user_topics = [topic.topic.id for topic in sorted(user.mentee.topics, key=lambda topic: topic.priority)]
 
     unordered_mentors = []
 
     for mentor in suitable_mentors:
-        mentor_topics = [topic.topic for topic in sorted(mentor.topics, key=lambda topic: topic.priority)]
+        mentor_topics = [topic.topic.id for topic in sorted(mentor.topics, key=lambda topic: topic.priority)]
         value = RBO(mentor_topics, user_topics)
-        unordered_mentors.append((mentor, value + ((mentor.score - 2.5)/10)))
+        unordered_mentors.append((mentor, value + ((mentor.score - 2.5)/300)))
 
     sorted_mentors = [mentorTuple[0] for mentorTuple in sorted(unordered_mentors, key=lambda tuple: tuple[1], reverse=True)]
-
 
     return sorted_mentors
 
@@ -24,8 +22,12 @@ def get_mentors(mentors, user):
 def RBO(list1, list2, p=0.7):
     value = 0
 
-    for d in range(1, min(len(list1), len(list2))+1):
-        Xd = overlap(list1, list2, d)
+    shorter, longer = (list1.copy(), list2.copy()) if len(list1) < len(list2) else (list2.copy(), list1.copy())
+    for _ in range(0, len(longer) - len(shorter)):
+        shorter.append(-1)
+
+    for d in range(1, len(shorter)+1):
+        Xd = overlap(shorter, longer, d)
         Ad = Xd/d
         value += (p**(d-1)) * Ad
 
